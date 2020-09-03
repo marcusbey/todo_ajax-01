d2c6e096f2f6a70795b119463b9d4843e2acede7
Romain BOBOE <rboboe@gmail.com>
2020-09-02 22:02:54 -0400

    adding detail view

--- api/views.py
+++ api/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import TaskSerializer
+from .models import Task
+
 # Create your views here.
 
 
@@ -18,7 +21,15 @@ def apiOverview(request):
     return Response(api_urls)
 
 
+@api_view(['GET'])
 def taskList(request):
     task = Task.objects.all()
-    serializer = TaskSerializer(tasks, many=True)
-    return Response()
+    serializer = TaskSerializer(task, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
