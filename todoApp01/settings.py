e87024ebe6fe0c1fe6b949e3a15f2c93174decf7
Romain BOBOE <rboboe@gmail.com>
2020-09-02 23:48:35 -0400

    adding static fils

--- todoApp01/settings.py
+++ todoApp01/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'page.apps.PageConfig',
     'api.apps.ApiConfig',
     'rest_framework',
 ]
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'todoApp01.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
